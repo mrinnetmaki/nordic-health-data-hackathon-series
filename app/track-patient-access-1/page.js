2f2b632aee616b118a28c011749a8188b6689fca
Mikael Rinnetmäki <mikael@sensotrend.com>
2025-12-14 12:53:42 +0200

    Redirect from page structure
    Directives in config do not work with static exports.

---
+++ app/track-patient-access-1/page.js
@@ -0,0 +1,5 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function PatientAccessRedirect() {
+  permanentRedirect('/2026/finland/patient-access');
+}
--- next.config.mjs
+++ next.config.mjs
@@ -2,32 +2,6 @@
 const nextConfig = {
   output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  async redirects() {
-    return [
-      // Basic redirect
-      {
-        source: '/q1-2026-finland',
-        destination: '/2026/finland',
-        permanent: true,
-      },
-      {
-        source: '/track-patient-access-1',
-        destination: '/2026/finland/patient-access',
-        permanent: true,
-      },
-      {
-        source: '/track-care-plans-1',
-        destination: '/2026/finland/care-plans',
-        permanent: true,
-      },
-      // Wildcard path matching
-      {
-        source: '/q1-2026-finland/:slug',
-        destination: '/2026/finland/:slug',
-        permanent: true,
-      },
-    ]
-  },  
 };
 
 export default nextConfig;
