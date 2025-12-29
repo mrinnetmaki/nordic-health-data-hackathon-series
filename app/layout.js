905b8534313789c8e8b93f0408312a615ef44502
Mikael Rinnetmäki <mikael@sensotrend.com>
2025-12-29 11:09:59 +0200

    Add favicons and og images

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,13 @@ export default function RootLayout({ children }) {
   console.log({ pathname });
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/hackathon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/hackathon/favicon.svg" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/hackathon/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Hackathon" />
+        <link rel="manifest" href="/hackathon/site.webmanifest" />
+      </head>
       <body className={pathname === '/2026' ?'' : 'finland' }>
         {children}
         <footer>
