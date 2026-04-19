84fa9214b345a8671e9997eb5c9c4eeacd94b1b1
Mikael Rinnetmäki <mikael@sensotrend.com>
2026-04-19 17:41:39 +0300

    Export with trailing slashes
    This is how we've built the links

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
